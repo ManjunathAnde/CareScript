e2153542136bac7daf6196580e35dc3965d54219
ManjunathAnde <manjunathande7@gmail.com>
2026-08-31 09:53:25 -0400

    createPatient test : comments V2

--- backend/functions/createPatient/index.test.js
+++ backend/functions/createPatient/index.test.js
@@ -14,15 +14,15 @@ beforeEach(() => { //Resetting configurations after every test
 });
 
 test('creates a patient with valid input', async () => {
-  ddbMock.on(UpdateCommand).resolves({ Attributes: { last_id: 7 } });
-  ddbMock.on(PutCommand).resolves({}); //Mock patient id is 7, which the code reads as current patient id
+  ddbMock.on(UpdateCommand).resolves({ Attributes: { last_id: 7 } }); //Mock patient id is 7, which the code reads as current patient id
+  ddbMock.on(PutCommand).resolves({}); ////Let the Put succeed with an empty response , nothing for code to consume
   const result = await handler({ 
     body: JSON.stringify({ name: 'John', age: 30, gender: 'Male' }),
   }); //Mock input from doctor going to the backend
 
   expect(result.statusCode).toBe(201); // Right input should return 201
   const patient = JSON.parse(result.body);
-  expect(patient.patient_id).toBe('W007'); // Patiend ID should be returned 7 acc to mock data
+  expect(patient.patient_id).toBe('W007'); // Patient ID should be returned 7 acc to mock data
   expect(patient.visit_count).toBe(0); // First visit= visit count is zero
   expect(patient.name).toBe('John'); 
 });
